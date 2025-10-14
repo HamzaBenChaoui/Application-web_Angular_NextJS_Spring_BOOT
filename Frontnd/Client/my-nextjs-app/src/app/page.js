6333355e0a79b1fd8c3a76a17dd615da9b190a70
HamzaBenchaoui <benchaouihamzaa@gmail.com>
2025-10-14 13:23:52 +0200

    ajouter components card list des produits

--- Frontnd/Client/my-nextjs-app/src/app/page.js
+++ Frontnd/Client/my-nextjs-app/src/app/page.js
@@ -3,6 +3,9 @@ import Footer from "./components/Footer";
 import RentabilisezBys from "./components/RentabilisezBys";
 import NewWaySection from "./landingpageComponent/page";
 import CarRentalHero from "./page_rentaal/CarRentalHero";
+import "./globals.css";
+import Card from "./components/Card";
+import { properties } from "./constants/data";
 export default function HomePage() {
   return (
     <div className="text-center mt-20">
@@ -10,8 +13,14 @@ export default function HomePage() {
        <NewWaySection/>
        <>
         <RentabilisezBys />
+          <div className="properties">
+        {properties.map((item) => (
+          <Card data={item} key={item.id} />
+        ))}
+      </div>
        <Actualise />
        </>
+     
        <Footer />
     </div>
   );
