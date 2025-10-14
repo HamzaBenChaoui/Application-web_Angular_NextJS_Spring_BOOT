b0f61f7421226651c135723599cb6fa256c6ff26
HamzaBenchaoui <benchaouihamzaa@gmail.com>
2025-10-14 12:46:30 +0200

    ajouter components actualis et footer et correction

--- Frontnd/Client/my-nextjs-app/src/app/page.js
+++ Frontnd/Client/my-nextjs-app/src/app/page.js
@@ -1,3 +1,5 @@
+import Actualise from "./components/Actualise";
+import Footer from "./components/Footer";
 import RentabilisezBys from "./components/RentabilisezBys";
 import NewWaySection from "./landingpageComponent/page";
 import CarRentalHero from "./page_rentaal/CarRentalHero";
@@ -5,9 +7,12 @@ export default function HomePage() {
   return (
     <div className="text-center mt-20">
       <CarRentalHero />
-   
-      <NewWaySection/>
+       <NewWaySection/>
+       <>
         <RentabilisezBys />
+       <Actualise />
+       </>
+       <Footer />
     </div>
   );
 }
